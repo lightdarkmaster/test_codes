eed1785dce7d1e308ef0e3aa57fde719537f832d
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-10-18 11:01:12 +0800

    Add Bubblesort java file

---
+++ bubblesort.java
@@ -0,0 +1,41 @@
+public class bubblesort {
+    public static void main(String[] args) {
+       
+        int[] myNum = {100, 220, 30, 140, 3, 28};
+        
+        // Call bubbleSort method to sort the array
+        bubbleSort(myNum);
+        
+        // Print the sorted array
+        System.out.println("Sorted Array:");
+        for (int num : myNum) {
+            System.out.print(num + " ");
+        }
+    }
+
+    // Method to perform Bubble Sort
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+        
+        // Outer loop to control the number of passes
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            
+            // Inner loop for comparing adjacent elements
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap the elements if they are in the wrong order
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped in the inner loop, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
