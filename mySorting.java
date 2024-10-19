83939a5affe342b8c7bea4b4b6c1dbbaf6d1d5a3
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-10-20 07:08:38 +0800

    Add Selection Sorting algorithm

--- mySorting.java
+++ mySorting.java
@@ -2,7 +2,8 @@
 public class mySorting {
 
     public static void main(String[] args) {
-        int num[] = { 23,56,1,78,12,90,33,67,2};    
+
+        int num[] = {23,56,1,78,12,90,33,67,2};    
 
         mySortPackage sort = new mySortPackage();
         sort.myBubbleSort(num);
