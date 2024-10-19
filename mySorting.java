4cf3c5eb8fb30b2ef9565fd38ac5739b7fe3e878
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-10-19 10:56:09 +0800

    Add instructions.txt

--- mySorting.java
+++ mySorting.java
@@ -5,7 +5,6 @@ public class mySorting {
         int num[] = { 23,56,1,78,12,90,33,67,2};    
 
         mySortPackage sort = new mySortPackage();
-
         sort.myBubbleSort(num);
         
         for( int nums : num){
