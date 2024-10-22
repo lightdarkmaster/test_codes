89c25c88027f0d5946440655f7f7475eb22b1980
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2024-10-22 19:32:25 +0800

    Updated code selection sorting algorithm

--- sSort.java
+++ sSort.java
@@ -1,8 +1,8 @@
-public class sSort {
+public class sSort{
+    
+    public static void main(String[] args){
 
-    public static void main(String[] args) {
-        
-        int myNum[] = {12,56,23,9,1,67,90,76,34,2};
+        int myNum[] = {12,56,89,345,2,0,23,1,67,3,7,4,33,23};
 
         selectionSort(myNum);
 
@@ -12,14 +12,13 @@ public class sSort {
     }
 
     public static void selectionSort(int[] array){
-        
+
         int arrayLength = array.length;
 
         for(int i=0; i < arrayLength - 1; i++){
             int minimumIndex = i;
 
             for(int j = i + 1; j < arrayLength; j++){
-
                 if(array[j] < array[minimumIndex]){
                     minimumIndex = j;
                 }
@@ -30,5 +29,4 @@ public class sSort {
             array[i] = temp;
         }
     }
-    
 }
